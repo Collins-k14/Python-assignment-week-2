41c30aac173c91589913b830cccad826529e7c72
Collins-k14 <collinskarani017@gmail.com>
2025-08-09 13:41:10 +0300

    initial commit

---
+++ my_list.py
@@ -0,0 +1,19 @@
+#empty list
+my_list = []
+#Append
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+#Insert value 15
+my_list.insert(1, 15)
+#Extend my_list
+my_list.extend([50,60,70])
+#Remove last element
+del my_list[-1]
+#sorting in ascending order
+my_list.sort()
+#Find and print the index of the value 30 in my_list
+index_30 = my_list.index(30)
+print("Index of 30:", index_30)
+print(my_list)
